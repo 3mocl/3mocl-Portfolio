c30d5daf97544db7bd94de20cce03784f344f50c
Clive Moras <3mocl@users.noreply.github.com>
2024-06-21 19:49:50 +0530

    Updates

---
+++ JS/color-mode.js
@@ -0,0 +1,42 @@
+// Define DOM elements
+
+const toggleButton = document.querySelector("#toggle-button");
+const root = document.querySelector(":root");
+const storageKey = "color-mode";
+const defaultMode = "light-mode";
+
+// Load the users prefrence
+function loadColorMode(){
+    const colorMode = localStorage.getItem(storageKey);
+    root.classList.add(colorMode || defaultMode );
+    updateToggleButton();
+}
+loadColorMode();
+
+// Toggle Button functionality
+toggleButton.addEventListener("click", ()=> {
+    saveColorMode();
+});
+
+// Saving the user's preference
+function saveColorMode()
+{
+    const currentMode = root.classList.contains("dark-mode") 
+    ? "light-mode"
+    : "dark-mode";
+    root.classList.remove("light-mode", "dark-mode");
+    root.classList.add(currentMode);
+    localStorage.setItem(storageKey, currentMode);
+    updateToggleButton();
+}
+
+function updateToggleButton()
+{
+    if(root.classList.contains("dark-mode")) {
+        toggleButton.style.backgroundImage = "var(--moon)"
+        }
+
+    else{
+        toggleButton.style.backgroundImage = "var(--sun)"
+    }
+}
